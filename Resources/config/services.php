81966b01b2a21bf5fb4025079ef1a090d118172b
baks.dev <admin@baks.dev>
2023-01-19 22:03:15 +0300

    Baks Development

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -14,17 +14,10 @@ return static function (ContainerConfigurator $configurator)
     
 	$namespace = 'BaksDev\Menu\Admin';
 	
-	$services->load($namespace.'\Controller\\', __DIR__.'/../../Controller')
-		->tag('controller.service_arguments');
-
     $services->load($namespace.'\Repository\\', __DIR__.'/../../Repository');
-    
-    //$services->load($namespace.'\UseCase\\', __DIR__.'/../../UseCase')
-    //  ->exclude('../../UseCase/**/*DTO.php');
-    
+
     $services->load($namespace.'\DataFixtures\\', __DIR__.'/../../DataFixtures')
       ->exclude(__DIR__.'/../../DataFixtures/**/*DTO.php');
-
-
+	
 };
 
