e6821dbcc698bfe7a4bffe8fe709fd37f4bac175
kingstar <admin@baks.dev>
2025-01-12 12:59:03 +0300

    Baks Development

--- Twig/MenuAdminExtension.php
+++ Twig/MenuAdminExtension.php
@@ -1,59 +1,44 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
- *
- *
  */
 
 namespace BaksDev\Menu\Admin\Twig;
 
+use BaksDev\Core\Twig\TemplateExtension;
 use BaksDev\Menu\Admin\Repository\MenuAdmin\MenuAdminInterface;
 use BaksDev\Menu\Admin\Repository\MenuAuthority\MenuAuthorityInterface;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\Token\SwitchUserToken;
+use BaksDev\Users\Profile\UserProfile\Repository\UserProfileTokenStorage\UserProfileTokenStorageInterface;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 final class MenuAdminExtension extends AbstractExtension
 {
-    private MenuAdminInterface $MenuAdmin;
-    private MenuAuthorityInterface $menuAuthority;
-    private TokenStorageInterface $tokenStorage;
-    private string $project_dir;
-
     public function __construct(
-        #[Autowire('%kernel.project_dir%')] string $project_dir,
-        MenuAdminInterface $repository,
-        MenuAuthorityInterface $menuAuthority,
-        TokenStorageInterface $tokenStorage,
-    )
-    {
-        $this->MenuAdmin = $repository;
-        $this->menuAuthority = $menuAuthority;
-        $this->project_dir = $project_dir;
-        $this->tokenStorage = $tokenStorage;
-    }
-
+        private readonly MenuAdminInterface $MenuAdmin,
+        private readonly MenuAuthorityInterface $menuAuthority,
+        private readonly UserProfileTokenStorageInterface $userProfileTokenStorage,
+        private readonly TemplateExtension $template,
+    ) {}
 
     public function getFunctions(): array
     {
@@ -67,37 +52,18 @@ final class MenuAdminExtension extends AbstractExtension
 
     public function renderMenuAdmin(Environment $twig): string
     {
+        $user = $this->userProfileTokenStorage->getUserCurrent();
+
         /** Меню навигации */
         $menu = $this->MenuAdmin->fetchAllAssociativeIndexed();
 
-        /** Меню доверенностей */
-        $token = $this->tokenStorage->getToken();
-        $user = $token instanceof SwitchUserToken ? $token->getOriginalToken()->getUser() : $token?->getUser();
+        // доверенные профили для быстрой смены
+        $authority = $user ? $this->menuAuthority->findAll($this->userProfileTokenStorage->getProfileCurrent()) : null;
 
-        /**
-         * Если авторизован администратор ресурса - подгружаем профили
-         */
-
-        $authority = null;
-
-        if($user)
-        {
-            $authority = $this->menuAuthority->findAll($user?->getProfile());
-        }
-
-        /** Подключаем если определен пользовательский шаблон */
-        if(file_exists($this->project_dir.'/templates/menu-admin/twig/menu.admin.html.twig'))
-        {
-            return $twig->render(
-                '@Template/menu-admin/twig/menu.admin.html.twig',
-                context: [
-                    'data' => $menu,
-                    'authority' => $authority
-                ]);
-        }
+        $path = $this->template->extends('@menu-admin:render_menu_admin/template.html.twig');
 
         return $twig->render(
-            '@menu-admin/twig/menu.admin.html.twig',
+            $path,
             context: [
                 'data' => $menu,
                 'authority' => $authority,
