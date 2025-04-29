7e8e24cc9bcbb69cdb41e3601f73449a8fe2a0bb
kingstar <admin@baks.dev>
2025-04-29 18:50:32 +0300

    Baks Development

--- Repository/MenuAdmin/MenuAdminRepository.php
+++ Repository/MenuAdmin/MenuAdminRepository.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -106,7 +106,6 @@ final readonly class MenuAdminRepository implements MenuAdminInterface
         $dbal->orderBy('section.sort', 'ASC');
 
         /* Кешируем результат DBAL */
-        return $dbal->enableCache('menu-admin', 3600)->fetchAllAssociativeIndexed();
-
+        return $dbal->enableCache('menu-admin', '1 day')->fetchAllAssociativeIndexed();
     }
 }
